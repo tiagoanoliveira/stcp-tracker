30d8ac3cd3093bbd84d27196867ce9c6927bd293
Tiago Oliveira <up202007448@up.pt>
2025-11-09 16:54:01 +0000

    Implement service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'stcp-live-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/busmap.html',
+  '/stop.html',
+  '/style.css',
+  './resources/favicon.svg',
+  './resources/header.js',
+  './realtime_stops/stopsData.js',
+  './realtime_stops/stopsMapApp.js'
+];
+
+// Instalação - cachear recursos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativação - limpar caches antigas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch - servir do cache quando offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
