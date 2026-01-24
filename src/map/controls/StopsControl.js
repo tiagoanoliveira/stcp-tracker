22d2add9fa20520a1d179cc6e0dcd2350ddcf952
Tiago Oliveira <geral@tiagoanoliveira.pt>
2026-01-24 21:44:10 +0000

    feat: create StopsControl for navigation to stops map

---
+++ src/map/controls/StopsControl.js
@@ -0,0 +1,41 @@
+/**
+ * StopsControl - Controlo personalizado para navegar para o mapa de paragens
+ */
+
+export function createStopsControl(map, stopsMapUrl = 'stopsmap_refactored.html') {
+  const StopsControl = L.Control.extend({
+    options: {
+      position: 'bottomleft'
+    },
+
+    onAdd: function(map) {
+      const container = L.DomUtil.create('div', 'leaflet-control-stops leaflet-bar leaflet-control');
+      
+      const button = L.DomUtil.create('button', '', container);
+      button.type = 'button';
+      button.title = 'Ver mapa de paragens';
+      button.innerHTML = `
+        <img src="./resources/paragem.png" alt="Ver paragens" />
+      `;
+
+      // Prevenir propagação de eventos do mapa
+      L.DomEvent.disableClickPropagation(button);
+      L.DomEvent.disableScrollPropagation(button);
+
+      // Handler do clique
+      L.DomEvent.on(button, 'click', function(e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+        window.location.href = stopsMapUrl;
+      });
+
+      return container;
+    },
+
+    onRemove: function(map) {
+      // Cleanup se necessário
+    }
+  });
+
+  return new StopsControl();
+}
