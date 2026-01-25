94ab59ea1ba6097837a7167352ca83c84ba06927
Tiago Oliveira <up202007448@up.pt>
2026-01-25 23:20:58 +0000

    Update default stopsMapUrl in createStopsControl

--- src/map/controls/StopsControl.js
+++ src/map/controls/StopsControl.js
@@ -2,7 +2,7 @@
  * StopsControl - Controlo personalizado para navegar para o mapa de paragens
  */
 
-export function createStopsControl(map, stopsMapUrl = 'stopsmap.html') {
+export function createStopsControl(map, stopsMapUrl = 'stopsmap') {
   const StopsControl = L.Control.extend({
     options: {
       position: 'bottomleft'
