a8a725959ec41eb5f589c4d86e1804be005ec316
Tiago Oliveira <up202007448@up.pt>
2026-03-08 16:30:33 +0000

    Update cache name to stcp-live-v6.1

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache estratégico para funcionamento offline
  */
 
-const CACHE_NAME = 'stcp-live-v6';
+const CACHE_NAME = 'stcp-live-v6.1';
 
 // Ficheiros essenciais para cachear
 const urlsToCache = [
