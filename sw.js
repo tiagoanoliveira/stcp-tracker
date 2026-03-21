be70b59acabccd7c0dd6710e31e65f74a4fc45fd
tiagoanoliveira <up202007448@up.pt>
2026-03-21 12:41:36 +0000

    Update sw

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache estratégico para funcionamento offline
  */
 
-const CACHE_NAME = 'stcp-live-v6.4';
+const CACHE_NAME = 'stcp-live-v6.5';
 
 // Ficheiros essenciais para cachear
 const urlsToCache = [
