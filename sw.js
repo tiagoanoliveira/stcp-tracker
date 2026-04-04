c9d1cfe4a4d444f935a8c5210c05c978d7a17225
Tiago Oliveira <up202007448@up.pt>
2026-04-04 11:17:49 +0100

    Update CACHE_NAME to version v3.0

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache estratégico para funcionamento offline
  */
 
-const CACHE_NAME = 'stcp-live-v6.6';
+const CACHE_NAME = 'stcp-live-v3.0';
 
 // Ficheiros essenciais para cachear
 const urlsToCache = [
