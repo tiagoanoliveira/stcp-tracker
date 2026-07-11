aa54ade7d973fdb777312ae3ae2bcc1ce295fe6a
Tiago Oliveira <up202007448@up.pt>
2026-07-11 16:42:45 +0100

    Update announcement message for special service

--- src/pages/BusMapApp.js
+++ src/pages/BusMapApp.js
@@ -82,7 +82,7 @@ export class BusMapApp {
       
       // Aviso temporário — STCP
       AnnouncementBanner.show(
-        'Serviço Especial gratuito Festival Comida Continente: procure a linha 38 no mapa ou "SE1" nas paragens.',
+        'Serviço Especial gratuito Festival Comida Continente: procure a linha 35 ou 38 no mapa ou SE1/SE2 nas paragens.',
         { type: 'warning', id: 'stcp-warning', dismissible: false }
       );
       
