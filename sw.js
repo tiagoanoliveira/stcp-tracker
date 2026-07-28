1a3a977b579db855bf4a693c33313d76a989f937
tiagoanoliveira <geral@tiagoanoliveira.pt>
2026-07-28 09:53:21 +0100

    Update sw

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache estratégico para funcionamento offline
  */
 
-const CACHE_NAME = 'stcp-live-v3.9.2';
+const CACHE_NAME = 'stcp-live-v3.9.4';
 
 // Ficheiros essenciais para cachear
 const urlsToCache = [
